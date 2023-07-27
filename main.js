5418ef2144afc6e77161bd30d38939d002f34c37
OneFatte <camiloandres798mail.com>
2023-07-26 21:02:07 -0500

    Se monta servidor en express

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hola");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+
